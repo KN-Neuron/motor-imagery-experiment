4232b560f3e4466a223e6c0f033ed2586012b7c7
M1KUS3Q <44113166+M1KUS3Q@users.noreply.github.com>
2026-02-27 22:12:18 +0100

    modular headset API + subscribers support (#4)
    * feat(egg_headset): refactor for usability and modularity; add support for BA MIDI
    
    * fix(egg_headset): reject non-eeg channels from sample reading
    
    * chore(egg_headset): add required simpleble.dll for runtime support
    
    * fix(egg_headset): fix wrong Optional import location
    
    * feat(egg_headset): add subscriber callbacks for eeg sample data; rename read_data() to poll() for standardization
    
    * feat(backend): add example; rename to __init__.py for clear imports

--- src/egg_headset/headset.py
+++ src/egg_headset/headset.py
@@ -1,17 +1,47 @@
-from . import EggHeadset
+import threading
+import time
 
-eeg = EggHeadset(port="COM4")  # Ustaw właściwy port
+from numpy import ndarray
+from egg_headset.drivers import MockDriver
+from egg_headset.model import HeadsetConfiguration, HeadsetModel
+from . import EggHeadset
+import numpy as np
+
+
+driver_config = HeadsetConfiguration(
+    model=HeadsetModel.MIDI_16CH_BASE, config_path="brainaccess_headsets_config.yaml"
+)
+# driver = BrainAccessDriver(driver_config)
+driver = MockDriver(driver_config)
+eeg = EggHeadset(driver)
 
 eeg.connect()
 eeg.start()
+eeg.annotate("start")
 
-run = True
-while run:
-    x = input("Wprowadź (exit aby zakończyć): ")
+samples = ndarray(shape=(driver_config.n_channels, 0), dtype=float)
+
+
+def poll_continuously() -> None:
+    while True:
+        eeg.poll()
+        time.sleep(0.05)  # 20 Hz polling rate
+
+
+threading.Thread(target=poll_continuously, daemon=True).start()
+
+while True:
+    x = input("Wprowadź adnotacje (exit aby zakończyć): ")
+
     if x == "exit":
-        run = False
-    else:
-        eeg.annotate(x)
+        break
+
+    output = eeg.get_output(seconds=1)
+    samples = np.concatenate((samples, output), axis=1)
+
+    eeg.annotate(x)
 
 eeg.stop()
-eeg.save("test.fif")
+
+
+print(samples)
