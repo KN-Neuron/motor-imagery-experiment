c1939b65a707cc4aee02eea0dc5bd9e0913017d1
glimpse <glimpse>
2026-04-25 17:56:57 +0200

    usunięcie niepotrzebnego main.py z roota projektu

--- main.py
+++
@@ -1,5 +0,0 @@
-import os
-os.add_dll_directory(r"C:\Program Files\BrainAccess\BrainAccess Board\dist\Lib\site-packages\brainaccess\lib")
-
-from brainaccess.core.eeg_manager import EEGManager
-mgr = EEGManager()
